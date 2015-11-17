68b94b76206f8d38de21431b28d4c4b21bf35af4
xiaotaozi1127 <xiaotaozi1127@hotmail.com>
2015-11-17 19:01:10 +0800

    taohui #parkingBoy: add my first test

---
+++ OOBootCamp/ParkingBoy/ParkingBoy.cs
@@ -0,0 +1,19 @@
+﻿using OOBootCamp.ParkingLot;
+
+namespace OOBootCamp.ParkingBoy
+{
+    public class ParkingBoy
+    {
+        Car _car;
+
+        public void Park(Car car)
+        {
+            _car = car;
+        }
+
+        public Car PickCar()
+        {
+            return _car;
+        }
+    }
+}
---
+++ TestClass/ParkingBoyFacts/ParkingBoyFacts.cs
@@ -0,0 +1,18 @@
+﻿using OOBootCamp.ParkingLot;
+using OOBootCamp.ParkingBoy;
+using Xunit;
+
+namespace TestClass.ParkingBoyFacts
+{
+    public class ParkingBoyFacts
+    {
+        [Fact]
+        public void should_pick_car_after_park_car()
+        {
+            var car = new Car("no1");
+            var parkingBoy = new ParkingBoy();
+            parkingBoy.Park(car);
+            Assert.Same(car, parkingBoy.PickCar());
+        }
+    }
+}
